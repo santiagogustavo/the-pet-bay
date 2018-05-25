ac036770fa502c79fab976fba0abd54d16a3a0ab
santiagogustavo <santiago.gustavo.mail@gmail.com>
2018-05-25 05:14:12 +0000

    (+) Implement more Navbar content, add images, remove unused files

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Container, LogoText } from './styles';
+import { Container, LogoText, Img, UserArea, FaIcon } from './styles';
+import Logo from '../../imgs/logo.png';
+import User from '../../imgs/user.png';
 
 const Navbar = () => (
   <Container>
     <LogoText to="/">
-      the pet bay
+      <Img src={Logo} />
+      <div>the pet bay</div>
     </LogoText>
+    <UserArea>
+      <FaIcon className="fas fa-paw" style={{ marginRight: 20 }} />
+      <Img src={User} />
+    </UserArea>
   </Container>
 );
 
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+import media from 'components/Utils/media';
+
 export const Container = styled.header`
   top: 0;
   width: 100%;
@@ -8,8 +10,13 @@ export const Container = styled.header`
   background: #3498db;
   color: #fafafa;
   display: flex;
-  align-content: flex-start;
+  align-content: center;
+  justify-content: center;
   box-shadow: #00000055 0px 0px 5px;
+  
+  ${media.medium`
+    align-content: flex-start;
+  `}
 `;
 
 export const LogoText = styled(Link)`
@@ -17,11 +24,51 @@ export const LogoText = styled(Link)`
   font-size: 25px;
   color: white;
   text-decoration: none;
-  margin: auto;
-  margin-left: 20px;
   transition: transform 0.2s ease;
+  display: flex;
+  align-items: center;
 
   &:hover {
     transform: scale(1.05);
   }
+
+  & > div {
+    display: none;
+    margin: 0;
+  }
+
+  ${media.medium`
+    margin: auto;
+    margin-left: 17.5%;
+    & > div {
+      display: block;
+      margin-left: 20px;
+    }
+  `}
+`;
+
+export const Img = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  transition: all 0.2s ease;
+`;
+
+export const UserArea = styled.div`
+  display: none;
+
+  ${media.medium`
+    float: right;
+    margin-right: 17.5%;
+    display: flex;
+    align-items: center;
+  `}
+`;
+
+export const FaIcon = styled.i`
+  position: relative;
+  font-size: 25px;
+  margin: auto;
+  line-height: 40px;
+  padding: 0px 5px;
 `;
