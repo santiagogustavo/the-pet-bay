23f0a593dc3644c7375e808702da8880881d8b93
santiagogustavo <santiago.gustavo.mail@gmail.com>
2018-07-02 06:16:49 +0000

    (+) Implement server side Services in landing page

--- src/actions/landingPage.js
+++ src/actions/landingPage.js
@@ -45,9 +45,61 @@ export const fetchPromotions = history => (dispatch) => {
       type: 'LANDING_PAGE/PROMOTIONS/FETCH/SUCCESS',
       payload: response.data,
     });
-    dispatch(fetchProducts(response.data.products));
+    dispatch(fetchProducts(response.data.products, history));
   }).catch(() => {
     dispatch(toggleFetchPromotions());
+    history.push('/500');
+  });
+};
+
+export const toggleFetchServices = () => ({
+  type: 'LANDING_PAGE/SERVICES/TOGGLE_FETCH',
+});
+
+export const toggleFetchService = index => ({
+  type: 'LANDING_PAGE/SERVICES/SERVICE/TOGGLE_FETCH',
+  index,
+});
+
+export const fetchService = (service, index, history) => (dispatch) => {
+  dispatch(toggleFetchService(index));
+  return axios({
+    method: 'GET',
+    url: `http://localhost:4000/services/${service}`,
+  }).then((response) => {
+    dispatch(toggleFetchService(index));
+    dispatch({
+      type: 'LANDING_PAGE/SERVICES/SERVICE/FETCH/SUCCESS',
+      payload: response.data,
+      index,
+    });
+  }).catch(() => {
+    dispatch(toggleFetchService(index));
+    history.push('/500');
+  });
+};
+
+export const fetchServicesList = (services, history) => (dispatch) => {
+  services.forEach(service => dispatch(fetchService(service.id, services.indexOf(service), history)));
+  return {
+    type: 'LANDING_PAGE/SERVICES/FETCH_SERVICES',
+  };
+};
+
+export const fetchServices = history => (dispatch) => {
+  dispatch(toggleFetchServices());
+  return axios({
+    method: 'GET',
+    url: 'http://localhost:4000/servicesLanding',
+  }).then((response) => {
+    dispatch(toggleFetchServices());
+    dispatch({
+      type: 'LANDING_PAGE/SERVICES/FETCH/SUCCESS',
+      payload: response.data,
+    });
+    dispatch(fetchServicesList(response.data, history));
+  }).catch(() => {
+    dispatch(toggleFetchServices());
     history.push('/500');
   });
 };
